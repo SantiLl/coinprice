f41aca2268950d3fed22e889049f3947c6aea86c
Santiago Llapur <santillapur@gmail.com>
2020-04-03 23:37:00 -0300

    Refactored api calls in pages controller

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'bootstrap', '~> 4.4.1'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# Not normal gems
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,19 @@
 require 'open-uri'
-require 'json'
+require 'nokogiri'
 
 class PagesController < ApplicationController
   def home
-    bitstamp = open("https://www.bitstamp.net/api/ticker/").string
-    bitstamp_doc = JSON.parse(bitstamp)
-    @bitstamp_price = bitstamp_doc["last"]
+    bitstamp = JSON.parse(open("https://www.bitstamp.net/api/ticker/").string)
+    @bitstamp_price = bitstamp["last"]
 
-    bitfinex = open("https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD").string
-    bitfinex_doc = JSON.parse(bitfinex)
-    @bitfinex_price = bitfinex_doc[0][7]
+    bitfinex = JSON.parse(open("https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD").string)
+    @bitfinex_price = bitfinex[0][7]
 
-    coinbase = open('https://api.coinbase.com/v2/prices/spot?currency=USD').string
-    coinbase_doc = JSON.parse(coinbase)
-    @coinbase_price = coinbase_doc["data"]["amount"]
+    coinbase = JSON.parse(open('https://api.coinbase.com/v2/prices/spot?currency=USD').string)
+    @coinbase_price = coinbase["data"]["amount"]
   end
 
   def about
   end
-
-  private
-
 
 end
